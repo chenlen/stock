1ac614cbea5905fe362e2501d3e1039778da87ab
Cheng Lei <attic.mascot@msa.hinet.net>
2025-05-12 21:37:49 +0800

    Remove use boost

--- LeiIA/Source/Key.h
+++ LeiIA/Source/Key.h
@@ -2,7 +2,9 @@
 
 #pragma once
 
-#include <boost/functional/hash.hpp>
+#include <string>
+#include <utility>
+#include <functional>
 
 namespace lei
 {
@@ -14,9 +16,9 @@ struct std::hash<std::pair<std::string, lei::DataFrequency>>
 {
     std::size_t operator()(const std::pair<std::string, lei::DataFrequency>& key) const noexcept
     {
-        std::size_t seed = 0;
-        boost::hash_combine(seed, key.first);
-        boost::hash_combine(seed, key.second);
-        return seed;
+        std::size_t h1 = std::hash<std::string>{}(key.first);
+        std::size_t h2 = std::hash<std::underlying_type_t<lei::DataFrequency>>{}(static_cast<std::underlying_type_t<lei::DataFrequency>>(key.second));
+
+        return h1 ^ (h2 + 0x9e3779b9 + (h1 << 6) + (h1 >> 2));
     }
 };
